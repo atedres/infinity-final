0e2295e6b442efbc3d7e4125caf0b00b75393691
ayman IT <aymanradi93@gmail.com>
2025-06-25 08:09:49 +0000

    hon the Home page make the header a little bigger

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -91,7 +91,7 @@ export default function Home() {
     <div className="flex min-h-screen flex-col">
        <TooltipProvider>
       <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-        <div className="container flex h-14 max-w-screen-2xl items-center px-4 md:px-6">
+        <div className="container flex h-16 max-w-screen-2xl items-center px-4 md:px-6">
             <Link href="/" className="mr-6 flex items-center space-x-2">
                 <InfinityIcon className="h-6 w-6 text-primary" />
                 <span className="font-bold sm:inline-block">
