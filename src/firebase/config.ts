4d3df2d32024bb1f46c5957563f8994c768db21b
ayman IT <aymanradi93@gmail.com>
2025-10-23 08:25:36 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "infinity-hub-xhnwm",
+  "appId": "1:1042134268607:web:2bc96c503ee30b4402640a",
+  "apiKey": "AIzaSyAs_w2BvLvy5r8muw1XlLrdJydYaGbm-X0",
+  "authDomain": "infinity-hub-xhnwm.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1042134268607"
+};
